206b86fc0e0ee06935c2f299858bd7454343eae0
Billy Li <billy1999fans@gmail.com>
2025-02-15 21:13:44 +0100

    Complete: 01-Fundamentals-Part-1

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,25 @@
+let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Billy";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let billy_mat = "BM";
+let $function = 27;
+
+let person = "billy";
+const PI = 3.1415;
+
+let myFirstJob = "Programmer";
+let myCurrentJob = "Teacher";
+
+let job1 = "programmer";
+let job2 = "teacher";
+
+console.log(myFirstJob);
