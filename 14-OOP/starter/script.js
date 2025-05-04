4eda8f652c09efda0859ca3e2ec4de6743ec6687
Billy Li <billy1999fans@gmail.com>
2025-05-04 11:44:47 +0200

    In Progress: Section 14: Object-Oriented Programming

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,1 +1,24 @@
 'use strict';
+
+const currentYear = new Date().getFullYear();
+
+// Constructor function
+const Person = function (firstName, birthYear) {
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+
+  // Never create a method inside a constructor function
+  //   this.calcAge = function () {
+  //     console.log(2037 - this.birthYear);
+  //   };
+};
+
+const billy = new Person('Billy', 1999);
+
+// Prototype
+Person.prototype.calcAge = function () {
+  console.log(currentYear - this.birthYear);
+};
+
+billy.calcAge();
+
